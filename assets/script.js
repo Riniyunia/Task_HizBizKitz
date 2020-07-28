72fc471a55bf1aeea1672a8e851a26fb61600899
Riniyunia <64077593+Riniyunia@users.noreply.github.com>
2020-07-28 12:46:04 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const anchors = document.getElementsByTagName("a");
+
+const feedbackAnchor = anchors[7];
+
+feedbackAnchor.addEventListener ('click', function(event) {
+    alert("My apologies. Our email address has not been yet set up. Kindly contact us via YouTube, IG, or Facebook. Thank you!");
+})
+
